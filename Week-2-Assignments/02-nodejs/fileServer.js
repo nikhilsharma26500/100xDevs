d4be9c921165795e651b3f8e35534266c4aae705
Nikhil <nikhilsharma26500@outlook.com>
2023-11-07 09:57:55 +0530

    adding week-2-assignment

---
+++ Week-2-Assignments/02-nodejs/fileServer.js
@@ -0,0 +1,54 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+
+    - For any other route not defined in the server return 404
+
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const directoryPath = path.join(__dirname, "./files");
+
+// let fileList = [];
+
+app.get("/files", (req, res) => {
+  fs.readdir(directoryPath, (err, fileList) => {
+    if (err) {
+      return req.status(404).send();
+    } else {
+      return req.status(200).json(fileList);
+    }
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filePath = path.join(__dirname, req.params.filename);
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if(err){
+      return res.status(404).send("File not found")
+    }else{
+      return res.status(200).send(data)
+    }
+  })
+});
+
+app.use((req, res) => {
+  return req.status(404);
+});
+
+module.exports = app;
